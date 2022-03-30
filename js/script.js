16129d675537475189126c0a7062091fa1f1edb0
Lori Jefitjian <lorijefitjiann@yahoo.com>
2022-03-30 15:05:00 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let skills
+
+const $button = $('button')
+const $ul = $('ul')
+const $input = $('input')
+
+$button.on("click", handleAddSkill)
+$ul.on("click", "span", handleDelete)
+
+init()
+
+function init() {
+  skills = []
+  render()
+}
+
+function handleAddSkill() {
+  const skill = $input.val()
+  if (skill) {
+    const $skill = $(`<li><span>X</span>${skill}</li>`)
+    skills.push($skill)
+    $input.val("")
+    render()
+  } else return
+}
+
+
+function handleDelete() {
+ 
+  $(this).closest('li').remove()
+  let currentSkill = $(this).closest('li')
+  skills.pop(currentSkill)
+
+}
+
+function render() {
+  if (!skills.length) $ul.css("margin-bottom", "30px")
+  else $ul.css("margin-bottom", "0px")
+
+  $ul.html(skills)
+}
